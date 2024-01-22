2cb5349b5765520696223179c2c1f590762df1f9
ARazzak <a.razzak12@gmail.com>
2024-01-22 15:38:18 +0600

    portfolio project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,36 @@
 (function ($) {
     "use strict";
 
+      /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+
+      }, true);
+    }
+
+  });
+
+
     // Spinner
     var spinner = function () {
         setTimeout(function () {
@@ -60,6 +90,8 @@
             '<i class="bi bi-chevron-right"></i>'
         ]
     });
+
+   
 
     
 })(jQuery);
